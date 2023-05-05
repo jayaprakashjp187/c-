9079031be8b18fd979665ff0f99f953cf11b7228
“jayaprakash” <jayaprakashjp187@gmail.com>
2023-05-05 17:30:56 +0530

    adding program file

---
+++ programming/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming
+{
+    internal class Human
+    {
+        public string name;
+        public float height;
+        public long phoneNumber;
+        public void HumanDetails()
+        {
+            Console.WriteLine("name:{0} height:{1} phoneNumber:{2}", name, height, phoneNumber);
+        }
+
+    }
+}
---
+++ programming/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Human jp = new Human();
+            Console.WriteLine("please enter your name");
+            jp.name = Console.ReadLine();
+            Console.WriteLine("please enter your height");
+            jp.height = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("please enter your phoneNumber");
+            jp.phoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            jp.HumanDetails();
+        }
+    }
+}
